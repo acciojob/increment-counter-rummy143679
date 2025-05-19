1e195d4be6c89ab337aa900d19164220ce56df3e
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-19 20:42:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const pt = document.getElementByTagName("p");
+const btn = document.getElementByTagName("button");
+
+btn.addEventListener("click", () => {
+	alert(pt.textContent);
+	pt.innerHtml = parseInt(pt.innerHtml) + 1;
+})
